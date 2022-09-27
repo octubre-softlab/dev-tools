9ada1a2683537e8e0c8f73d79e5a8a1afd267726
perez.e <perez.e@softlab.umet.edu.ar>
2022-09-27 11:38:46 -0300

    feat: Get-EnvFile

--- azure-appconfigs/Get-EnvFile.ps1
+++ azure-appconfigs/Get-EnvFile.ps1
@@ -1,17 +1,43 @@
+function ConvertTo-Env {
+    [CmdletBinding(RemotingCapability='None')]
+    param(
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [hashtable] ${InputObject}
+    )
+    process
+    {
+        $sb = [System.Text.StringBuilder]::new()
+        $InputObject.Keys | ForEach-Object {
+            $key = $_.Replace(":","__")
+            $value = $InputObject[$_].Replace("`n","\n")
+            [void]$sb.AppendLine("${key}=`"${value}`"")
+        }
+        return $sb.ToString()
+    }
+}
+
+
+$AppConfigStoreName = $args[0]
+If(-Not $AppConfigStoreName) {
+    $AppConfigStoreName = Read-host "Ingrese el Azure App Configuration Store name"
+}
+
+
 # az login
 
 az configure --defaults appconfig_auth_mode="login"
 
 Write-Output "Cargando..."
 
-$keyvalues = az appconfig kv list -n "appcs-sharedconf-prd-ue" --fields key label --all | ConvertFrom-Json
+$keyvalues = az appconfig kv list -n $AppConfigStoreName --fields key label --all | ConvertFrom-Json
 
 # Seleccionar app
 
-$apps = $keyvalues | ForEach-Object { 
+$apps = @($keyvalues | ForEach-Object { 
                         ($_.key -Split ":")[0] 
-                    } | Select-Object -Unique
+                    } | Select-Object -Unique)
 
+Write-Output ""
 Write-Output "Aplicaciones:"
 
 $apps | ForEach-Object {
@@ -35,15 +61,17 @@ until (($chosenNumber -ge 1 -and $chosenNumber -le $apps.Count) -and $numOK)
 
 $selectedApp = $apps[$chosenNumber - 1];
 
-Write-Output "Aplicación seleccionada: $selectedApp"
+Write-Output "Aplicación seleccionada: "
+Write-Host "$selectedApp" -ForegroundColor Green
 
 If($selectedApp -Eq "webapi") {
 
     # Seleccionar tenant
+    Write-Output ""
     Write-Output "Tenants:"
-    $tenants = $keyvalues | Where-Object { $_.key -Like "${selectedApp}:*" }
+    $tenants = @($keyvalues | Where-Object { $_.key -Like "${selectedApp}:*" }
                           | ForEach-Object {  ($_.key -Split ":")[1] } 
-                          | Select-Object -Unique
+                          | Select-Object -Unique)
 
     $tenants = $tenants | Where-Object { $_ -inotmatch "@" -And $_ -Ne "Common" }
 
@@ -67,16 +95,21 @@ If($selectedApp -Eq "webapi") {
 
     $selectedTenant = $tenants[$chosenNumber - 1];
 
-    Write-Output "Tenant seleccionado: $selectedApp $selectedTenant"
+    Write-Output "Tenant seleccionado:"
+    Write-Host "$selectedApp $selectedTenant" -ForegroundColor Green
+
 
     $selectedApp = "${selectedApp}:${selectedTenant}"
 }
 
 # Seleccionar environment
 
-$environments = $keyvalues | Where-Object { $_.key -Like "${selectedApp}:*" }
+Write-Output ""
+Write-Output "Ambientes:"
+
+$environments = @($keyvalues | Where-Object { $_.key -Like "${selectedApp}:*" }
                           | ForEach-Object { $_.label } 
-                          | Select-Object -Unique
+                          | Select-Object -Unique)
 
 $environments | ForEach-Object {
     $index = $environments.IndexOf($_)
@@ -89,7 +122,7 @@ $environments | ForEach-Object {
 do {
     try {
         $numOk = $true
-        [int]$chosenNumber = Read-host "Seleccione un environment"
+        [int]$chosenNumber = Read-host "Seleccione un ambiente"
     } # end try
     catch {
         $numOK = $false
@@ -99,12 +132,14 @@ until (($chosenNumber -ge 1 -and $chosenNumber -le $environments.Count) -and $nu
 
 $selectedEnv = $environments[$chosenNumber - 1]
 
-Write-Output "Environment seleccionado: $selectedApp $selectedEnv"
+Write-Output "Environment seleccionado:"
+Write-Host "$selectedApp $selectedEnv" -ForegroundColor Green
 
+Write-Output ""
 Write-Output "Cargando secrets y configs..."
 
 $tenantKeyvalues = az appconfig kv list `
-                        --name "appcs-sharedconf-prd-ue" `
+                        --name $AppConfigStoreName `
                         --key "${selectedApp}:*" `
                         --label "$selectedEnv",\0 `
                         --resolve-keyvault `
@@ -113,11 +148,35 @@ $tenantKeyvalues = az appconfig kv list `
 
 $result = @{}
 
-$tenantKeyvalues | ForEach-Object {
+If($selectedApp -Like "webapi:*") { 
+    $commonTenantKeyvalues = az appconfig kv list `
+                        --name $AppConfigStoreName `
+                        --key "webapi:Common:*" `
+                        --label "$selectedEnv",\0 `
+                        --resolve-keyvault `
+                        --all
+                    | ConvertFrom-Json
+
+    $commonTenantKeyvalues | Sort-Object -Property Label | ForEach-Object {
+        $entryName = $_.Key.Replace("webapi:Common:","")
+        $result[$entryName] = $_.Value
+    }
+}
+
+$tenantKeyvalues | Sort-Object -Property Label | ForEach-Object {
     $entryName = $_.Key.Replace("${selectedApp}:","")
     $result[$entryName] = $_.Value
 }
 
 $json = $result | ConvertTo-Json
 
+$envFile = $result | ConvertTo-Env
+
+
+Write-Output ""
+Write-Host "JSON File" -ForegroundColor Green
 Write-Output $json
+
+Write-Output ""
+Write-Host "Env File" -ForegroundColor Green
+Write-Output $envFile
---
+++ azure-appconfigs/README.md
@@ -0,0 +1,50 @@
+Este script sirve para obtener secret.json o .env files a partir
+
+1. Descargar script
+
+```powershell
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+(New-Object -TypeName System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/octubre-softlab/dev-tools/main/azure-appconfigs/Get-EnvFile.ps1", "$PWD/Get-EnvFile.ps1")
+```
+
+2. Hacer login en azure: `az login`
+
+3. Ejecutar script `./Get-EnvFile.ps1 nombre-de-appconfigstore`
+
+```
+.\Get-EnvFile.ps1
+Cargando...
+
+Aplicaciones:
+1. Hola
+2. Webapi
+Seleccione una aplicación: 2
+Aplicación seleccionada: 
+webapi
+
+Tenants:
+1. Arrabaleros
+2. ElTrineo
+3. FATERYH
+4. GUAU
+5. ISSA
+6. MEPADIP
+7. OctubreOSPERYH
+8. OctubreOSPERYHRA2
+9. OctubreOSPERYHRA
+10. SERACARH
+11. SIGA
+12. SUTERH
+Seleccione un tenant: 1
+Tenant seleccionado:
+webapi Arrabaleros
+
+Ambientes:
+1. Production
+2. Test
+Seleccione un ambiente: 2
+Environment seleccionado:
+webapi:Arrabaleros Test
+
+Cargando secrets y configs...
+```
